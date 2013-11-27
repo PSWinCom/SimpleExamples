be71aafff2fee609c1dbbdfab4d8e693608b6e62
audun@pswin.com <audun@pswin.com>
2013-11-27 16:47:24 +0100

    Added node.js example

---
+++ sendmessage.node.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var post_data = "<?xml version=\"1.0\"?>\
+  <SESSION>\
+    <CLIENT>myusername</CLIENT>\
+    <PW>mypassword</PW>\
+    <MSGLST>\
+      <MSG>\
+        <TEXT>Test message</TEXT>\
+        <RCV>4799999999</RCV>\
+      </MSG>\
+    </MSGLST>\
+  </SESSION>";
+
+var post_req = http.request(
+  {
+    host: "gw2-fro.pswin.com",
+    port: "82",
+    method: "POST",
+    header: {
+      'Content-Type': 'application/xml',
+      'Content-Length': post_data.length
+    }
+  });
+
+post_req.write(post_data);
+post_req.end();
